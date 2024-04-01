ee7f034701e06583e019cbe3c261558cc71c5343
Luke OSBORNE <losborne@mantu.com>
2024-04-01 11:47:16 +0100

    feat: setup db connection

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BankingApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection.Metadata;
+
+namespace BankingApi
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<BankAccount> BankAccounts { get; set; }
+    }
+}
---
+++ Models/BankAccount.cs
@@ -0,0 +1,9 @@
+﻿namespace BankingApi.Models
+{
+    public class BankAccount
+    {
+        public int Id { get; set; }
+        public int Balance {  get; set; }
+        public int UserId { get; set; }
+    }
+}
