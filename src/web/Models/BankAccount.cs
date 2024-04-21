76b4316ae145a22b0dc1b5628f39c37e521155bf
Luke OSBORNE <losborne@mantu.com>
2024-04-21 13:39:26 +0100

    feat: restructure

--- src/web/Models/BankAccount.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Banking.Models
-{
-    public class BankAccount
-    {
-        public int Id { get; set; }
-        public int Balance {  get; set; }
-        public int UserId { get; set; }
-    }
-}
